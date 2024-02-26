d12ae6f2064727206d7bb54bd7eb7cee7c23d09d
Richy-irad <richardirad@gmail.com>
2024-02-26 15:29:21 -0500

    add the contact details, minus the google maps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import heroBackground from "../../public/hero-background.jpg";
 
 import Service from "@/components/service";
 import Project from "@/components/project";
+import ContactDetails from "@/components/contact-details";
 
 import { services } from "@/lib/services";
 import { projects } from "@/lib/projects";
@@ -17,6 +18,7 @@ export default function Home() {
     <main className="h-full">
       {/* hero */}
       <div className="flex items-center h-[818px] bg-dark-200/50">
+        {/* the background image */}
         <div className="relative w-full h-full">
           <div className="absolute -z-10 w-full h-full">
             <Image
@@ -28,6 +30,7 @@ export default function Home() {
             />
           </div>
         </div>
+        {/* the content */}
         <div className="absolute flex flex-col gap-16 items-center w-full">
           <div className="flex flex-col gap-6 text-center text-white">
             <h3 className="text-xl font-medium uppercase flex gap-6 items-center self-center">
@@ -143,6 +146,36 @@ export default function Home() {
       </div>
 
       {/* contact us */}
+      <div className="flex items-center w-full h-[790px] bg-dark-200/75">
+        {/* the background image */}
+        <div className="relative w-full h-full">
+          <div className="absolute -z-10 w-full h-full">
+            <Image
+              src={heroBackground}
+              alt="hero background image"
+              sizes="100vw"
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </div>
+        </div>
+        {/* the content */}
+        <div className="absolute flex flex-col gap-16 items-center w-full">
+          <div className="flex flex-col gap-6 text-center text-white">
+            <h3 className="text-xl font-medium uppercase flex gap-6 items-center self-center">
+              <hr className="block w-[53px] h-[3px] bg-foreground-100 rounded-full" />
+              get in touch with us
+              <hr className="block w-[53px] h-[3px] bg-foreground-100 rounded-full" />
+            </h3>
+            <h1 className="w-[405px] text-3.5xl font-bold capitalize">
+              your project is much simpler than you think.
+            </h1>
+          </div>
+
+          {/* the tabs */}
+          <ContactDetails />
+        </div>
+      </div>
     </main>
   );
 }
---
+++ src/components/contact-details.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import { Fragment } from "react";
+
+import { Tab } from "@headlessui/react";
+import { Envelope, Phone, MapPin } from "@phosphor-icons/react";
+import clsx from "clsx";
+
+// import Map from "./map";
+
+const contacts = [
+  {
+    icon: <Envelope size={24} weight="bold" className="text-white" />,
+    text: "grosorinc2@gmail.com",
+    descriptor: "email",
+  },
+  {
+    icon: <Phone size={24} weight="bold" className="text-white" />,
+    text: "(514) 57205067",
+    descriptor: "phone",
+  },
+  {
+    icon: <MapPin size={24} weight="bold" className="text-white" />,
+    text: "60 Rue du Docteur-Lortie, St. Polycarpe, J0P1X0, QC",
+    descriptor: "our office",
+  },
+];
+
+export default function ContactDetails() {
+  return (
+    <Tab.Group vertical>
+      <div className="flex gap-5 w-[894px]">
+        {/* tabs list */}
+        <Tab.List className="basis-1/4 grow-0 shrink-0 flex flex-col">
+          <Tab as={Fragment}>
+            {({ selected }) => (
+              <button
+                type="button"
+                className={clsx(
+                  "capitalize px-7 py-4 text-light-100 hover:text-white hover:bg-foreground-200/50 outline-none text-start w-full",
+                  {
+                    "text-white border-l-4 border-foreground-100 bg-foreground-200":
+                      selected,
+                    "border-l-0.5 border-light-200": !selected,
+                  }
+                )}
+              >
+                Contact details
+              </button>
+            )}
+          </Tab>
+          <Tab as={Fragment}>
+            {({ selected }) => (
+              <button
+                type="button"
+                className={clsx(
+                  "capitalize px-7 py-4 text-light-100 hover:text-white hover:bg-foreground-200/50 outline-none text-start w-full",
+                  {
+                    "text-white border-l-4 border-foreground-100 bg-foreground-200":
+                      selected,
+                    "border-l-0.5 border-light-200": !selected,
+                  }
+                )}
+              >
+                directions
+              </button>
+            )}
+          </Tab>
+        </Tab.List>
+        {/* tabs panels */}
+        <Tab.Panels>
+          <Tab.Panel>
+            <div className="flex flex-col gap-6">
+              {contacts.map((contact, index) => (
+                <div key={index} className="flex gap-4 items-center">
+                  <div className="p-2 5 rounded bg-foreground-200 text-white">
+                    {contact.icon}
+                  </div>
+                  <div className="flex flex-col gap-2 w-full">
+                    <p className="text-white font-semibold">{contact.text}</p>
+                    <p className="text-light-200 font-normal capitalize">
+                      {contact.descriptor}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </Tab.Panel>
+
+          <Tab.Panel>
+            <p>Directions</p>
+          </Tab.Panel>
+        </Tab.Panels>
+      </div>
+    </Tab.Group>
+  );
+}
