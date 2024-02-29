44c4662f4984438b490280d7b686a128ae278ec1
Richy-irad <richardirad@gmail.com>
2024-02-29 13:13:01 -0500

    redesign contacts and remove maps untill figured out how to use it

--- src/components/contact-details.tsx
+++ src/components/contact-details.tsx
@@ -1,12 +1,6 @@
 "use client";
 
-import { Fragment } from "react";
-
-import { Tab } from "@headlessui/react";
 import { Envelope, Phone, MapPin } from "@phosphor-icons/react";
-import clsx from "clsx";
-
-// import Map from "./map";
 
 const contacts = [
   {
@@ -28,72 +22,24 @@ const contacts = [
 
 export default function ContactDetails() {
   return (
-    <Tab.Group vertical>
-      <div className="flex flex-col md:flex-row gap-5 w-full px-5 lg:w-[894px]">
-        {/* tabs list */}
-        <Tab.List className="basis-1/4 grow-0 shrink-0 flex flex-col">
-          <Tab as={Fragment}>
-            {({ selected }) => (
-              <button
-                type="button"
-                className={clsx(
-                  "capitalize px-7 py-4 md:text-light-100 hover:text-white hover:bg-foreground-200/50 outline-none text-start w-full",
-                  {
-                    "text-white border-l-4 border-foreground-100 bg-foreground-200":
-                      selected,
-                    "border-l-0.5 border-light-200": !selected,
-                  }
-                )}
-              >
-                Contact details
-              </button>
-            )}
-          </Tab>
-          <Tab as={Fragment}>
-            {({ selected }) => (
-              <button
-                type="button"
-                className={clsx(
-                  "capitalize px-7 py-4 md:text-light-100 hover:text-white hover:bg-foreground-200/50 outline-none text-start w-full",
-                  {
-                    "text-white border-l-4 border-foreground-100 bg-foreground-200":
-                      selected,
-                    "border-l-0.5 border-light-200": !selected,
-                  }
-                )}
-              >
-                directions
-              </button>
-            )}
-          </Tab>
-        </Tab.List>
-        {/* tabs panels */}
-        <Tab.Panels>
-          <Tab.Panel>
-            <div className="flex flex-col gap-6">
-              {contacts.map((contact, index) => (
-                <div key={index} className="flex gap-4 items-center">
-                  <div className="p-2 5 rounded bg-foreground-200 text-white">
-                    {contact.icon}
-                  </div>
-                  <div className="flex flex-col gap-2 w-full">
-                    <p className="text-dark-100 md:text-white font-semibold">
-                      {contact.text}
-                    </p>
-                    <p className="text-dark-200 md:text-light-200 font-normal capitalize">
-                      {contact.descriptor}
-                    </p>
-                  </div>
-                </div>
-              ))}
+    <div className="flex flex-col items-center w-full px-5 lg:w-[894px]">
+      <div className="flex flex-col gap-6">
+        {contacts.map((contact, index) => (
+          <div key={index} className="flex gap-4 items-center">
+            <div className="p-2 5 rounded bg-foreground-200 text-white">
+              {contact.icon}
             </div>
-          </Tab.Panel>
-
-          <Tab.Panel>
-            <p>Directions</p>
-          </Tab.Panel>
-        </Tab.Panels>
+            <div className="flex flex-col gap-2 w-full">
+              <p className="text-dark-100 md:text-white font-semibold">
+                {contact.text}
+              </p>
+              <p className="text-dark-200 md:text-light-200 font-normal capitalize">
+                {contact.descriptor}
+              </p>
+            </div>
+          </div>
+        ))}
       </div>
-    </Tab.Group>
+    </div>
   );
 }
